5b6c03253d06ae73543993bccc41c73f1f88c849
rbeau12 <107488237+rbeau12@users.noreply.github.com>
2024-11-05 21:29:36 +0000

    Done

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,39 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+  def directors
+    render({:template=>"misc_templates/directors"})
+  end
+  def movies
+      render({:template=>"misc_templates/movies"})
+  end
+  def actors
+      render({:template=>"misc_templates/actors"})
+  end
+  def youngest
+    @youngest_director=Director.where.not({ :dob => nil }).all.order({ :dob => :desc }).at(0)
+    render({:template=>"misc_templates/youngest"})
+  end
+  def eldest
+    @eldest_director=Director.all.order({ :dob => :asc }).at(0)
+    render({:template=>"misc_templates/eldest"})
+  end
+  def directorPage
+    dID=params.fetch("the_id")
+    @director=Director.where(:id=>dID)[0]
+    render({:template=>"misc_templates/dPage"})
+  end
+  def moviePage
+    mID=params.fetch("the_id")
+    @movie=Movie.where(:id=>mID)[0]
+    @directoru=Director.where({:id=>@movie.director_id})[0]
+
+    render({:template=>"misc_templates/mPage"})
+  end
+  def actorPage
+    dID=params.fetch("the_id")
+    @actor=Actor.where(:id=>dID)[0]
+    @characters=Character.where({:actor_id=>dID})
+    render({:template=>"misc_templates/aPage"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors",{:controller=>"misc",:action=>"directors" })
+  get("/movies",{:controller=>"misc",:action=>"movies"})
+  get("/actors",{:controller=>"misc", :action=>"actors"})
+  get("/directors/youngest",{:controller=>"misc",:action=>"youngest" })
+  get("/directors/eldest",{:controller=>"misc",:action=>"eldest" })
+  get("/directors/:the_id",{:controller=>"misc",:action=>"directorPage" })
+  get("/movies/:the_id",{:controller=>"misc",:action=>"moviePage"})
+  get("/actors/:the_id",{:controller=>"misc", :action=>"actorPage"})
 end
